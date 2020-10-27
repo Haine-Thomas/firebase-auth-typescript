c765c997d29d61b5c7ad13d63c346de80e7d2a0b
Mushi2216 <thomahaine@gmail.com>
2020-10-27 11:00:56 +0100

    Homepage done

---
+++ src/components/pages/Homepage.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+
+const Homepage: FC = () => {
+    return(
+        <section className="section">
+            <div className="container">
+                <h1 className="title has-text-centered is-size-1 mb-6">Welcome</h1>
+                <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo eligendi sit voluptate possimus totam quae non nulla repudiandae. Nobis, minus voluptas. Accusantium officiis, quod distinctio voluptas provident molestiae veritatis. Iste.</h2>
+            </div>
+        </section>
+    )
+}
